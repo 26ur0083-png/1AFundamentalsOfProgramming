918f55500046cd96001e74204fa23b4971a0a048
26ur0083-png <26ur0083@psu.edu.ph>
2026-09-22 20:47:13 +0800

    Updated Java code

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main (String []args){
         System.out.println ("Claudine Nillo Camaya");
         System.out.println ("Arwel Apilado Guce");
+        System.out.println ("Gwen Guillermo");
         System.out.println ("BSIT 1A");
     }
 }
