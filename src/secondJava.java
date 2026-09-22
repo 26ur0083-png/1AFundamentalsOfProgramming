6cb16a8c73299612c46b60c31e61ca9f2d298547
26ur0083-png <26ur0083@psu.edu.ph>
2026-09-22 19:03:32 +0800

    Initial commit

---
+++ src/secondJava.java
@@ -0,0 +1,17 @@
+public class secondJava {}
+ public static void main (String []args) {
+    String firstName = "Claudine";
+    String lastName= "Camaya";
+    String Address = "San Vicente West, Urdaneta City";
+    String Contactnumber = "09293893200";
+    int Age = 18;
+    String Crush = "Wala po";
+
+    System.out.println(firstName + " "+lastName);
+    System.out.println(Address);
+    System.out.println(Contactnumber);
+    System.out.println(Age);
+    System.out.println(Crush);
+}
+
+
